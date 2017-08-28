c05a7df5b2503327457417613060a690479f94c1
Dirk Peters <rocksandwoks@gmail.com>
2017-08-28 15:54:45 -0400

    Convert CSS #app tags to #root tags

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import marked from 'marked';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class Previewer extends Component {
@@ -56,9 +56,9 @@ const LegendButton = ({handleLegendChange}) =>
 
 const Legend = ({handleLegendChange}) =>
   <div className="legend">
-  <div className="close-legend" onClick={handleLegendChange}>&#10006;</div>
+    <div className="close-legend" onClick={handleLegendChange}>&#10006;</div>
     <h2 className="legend-title">Legend</h2>
-      <div className="row-define-markdown">
+    <div className="row-define-markdown">
       <div className="column define-markdown">
         <h3>What is Markdown?</h3>
         <blockquote cite="https://en.wikipedia.org/wiki/Markdown">Markdown is a lightweight markup language with plain text formatting syntax. It is designed so that it can be converted to HTML and many other formats using a tool by the same name.</blockquote>
@@ -67,41 +67,41 @@ const Legend = ({handleLegendChange}) =>
         <h3>Why should I be excited?</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet risus a nisi facilisis congue. Donec condimentum sollicitudin lectus id congue. Nullam ultricies pulvinar tincidunt. Etiam vestibulum ut lectus eget cursus.</p>
       </div>
+    </div>
+    <div className="column example-input">
+      <div className="example-text">
+        <p>This is a heading 1</p>
+        <p>===================</p>
       </div>
-      <div className="column example-input">
-        <div className="example-text">
-          <p>This is a heading 1</p>
-          <p>===================</p>
-        </div>
-        <div className="example-text">
-          <p>This is a heading 2</p>
-          <p>-------------------</p>
-        </div>
-        <div className="example-text">
-          <p>[This is a hyperlink](http://www.example.com)</p>
-        </div>
-        <div className="example-text">
-          <p>Italics work like *this* or like _this_.</p>
-        </div>
-        <div className="example-text">
-          <p>Bold text works like **this** or like __this__</p>
-        </div>
-        <div className="example-text">
-          <p>> This is a blockquote</p>
-        </div>
-        <div className="example-text">
-          <p>Make a horizontal rule with --- or ***</p>
-        </div>
+      <div className="example-text">
+        <p>This is a heading 2</p>
+        <p>-------------------</p>
       </div>
-      <div className="column">
-        <div className="example-text">
-          <h1>This is a heading 1</h1>
-        </div>
-        <div className="example-text">
-          <h2>This is a heading 2</h2>
-        </div>
-        <div className="example-text">
-          <a href="#">This is a hyperlink</a>
+      <div className="example-text">
+        <p>[This is a hyperlink](http://www.example.com)</p>
+      </div>
+      <div className="example-text">
+        <p>Italics work like *this* or like _this_.</p>
+      </div>
+      <div className="example-text">
+        <p>Bold text works like **this** or like __this__</p>
+      </div>
+      <div className="example-text">
+        <p>> This is a blockquote</p>
+      </div>
+      <div className="example-text">
+        <p>Make a horizontal rule with --- or ***</p>
+      </div>
+    </div>
+    <div className="column">
+      <div className="example-text">
+        <h1>This is a heading 1</h1>
+      </div>
+      <div className="example-text">
+        <h2>This is a heading 2</h2>
+      </div>
+      <div className="example-text">
+        <a href="#">This is a hyperlink</a>
         </div>
         <div className="example-text">
           <p>Italics work like <span className="italic">this</span> or like <span className="italic">this.</span></p>
@@ -137,7 +137,8 @@ const Output = ({text}) =>
     <h2>This is the Output.</h2>
     <div
       className="textarea-output"
-      dangerouslySetInnerHTML={{__html: marked(text, {breaks: true})}} />
+      dangerouslySetInnerHTML={{__html: marked(text, {breaks: true})}}
+    />
   </div>
 
 Previewer.propTypes = {

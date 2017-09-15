dd6b5aeb78535e694d6015d26718d77d33e03c7a
Dirk Peters <rocksandwoks@gmail.com>
2017-09-15 10:22:37 -0400

    Add imports/exports to LegendButton

--- src/components/LegendButton.js
+++ src/components/LegendButton.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const LegendButton = ({handleLegendChange}) =>
   <button className="legend-button" onClick={handleLegendChange}>
     Show Legend
   </button>
+
+LegendButton.propTypes = {
+  handleLegendChange: PropTypes.func.isRequired
+}
 
 export default LegendButton;

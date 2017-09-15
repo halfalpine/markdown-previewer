c6755ce72381532a92f8ce3e76fb7c30346d7a28
Dirk Peters <rocksandwoks@gmail.com>
2017-09-15 10:18:42 -0400

    Add imports/exports to Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const Input = ({text, handleInputChange}) =>
   <div className="md-component">
     <h2>This is the Input.</h2>
@@ -11,5 +14,10 @@ const Input = ({text, handleInputChange}) =>
       </textarea>
     </form>
   </div>
+
+Input.propTypes = {
+  text: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired
+}
 
 export default Input;
--- src/reducers/previewer.js
+++ src/reducers/previewer.js
@@ -18,4 +18,4 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-export default reducer'
+export default reducer;

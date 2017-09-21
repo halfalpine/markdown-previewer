bb033082013e503ec8976e281fb3dfebaf7f8074
Dirk Peters <rocksandwoks@gmail.com>
2017-09-21 12:22:57 -0400

    Add inline styles to Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Title = () => (
-  <div className="page-title">
-    <h1>Markdown Previewer</h1>
-    <p>A React project for <a href="https://www.freecodecamp.org/challenges/build-a-markdown-previewer">Free Code Camp</a></p>
-  </div>
-)
+const Title = () => {
+  const styles = {
+    pageTitle: {
+      width: "100%",
+      margin: "30px 0",
+      fontSize: "1.2em"
+    },
+    h1: {
+      textAlign: "center",
+      marginBottom: "0px"
+    },
+    p: {
+      textAlign: "center"
+    }
+  };
+  return (
+    <div className="page-title" style={styles.pageTitle}>
+      <h1 style={styles.h1}>Markdown Previewer</h1>
+      <p style={styles.p}>
+        A React project for{" "}
+        <a href="https://www.freecodecamp.org/challenges/build-a-markdown-previewer">
+          Free Code Camp
+        </a>
+      </p>
+    </div>
+  );
+};
 
 export default Title;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import reducer from './reducers/index';
-import './App.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducer from "./reducers/index";
+// import './App.css';
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 let store = createStore(reducer);
 
@@ -14,6 +14,6 @@ render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();

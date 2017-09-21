14e569c34da26f5bedb862748d2234ad24a5594b
Dirk Peters <rocksandwoks@gmail.com>
2017-09-21 12:48:55 -0400

    Add styles to Legend

--- src/components/LegendButton.js
+++ src/components/LegendButton.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const LegendButton = ({handleLegendChange}) =>
-  <button className="legend-button" onClick={handleLegendChange}>
-    Show Legend
-  </button>
+const LegendButton = ({ handleLegendChange }) => {
+  const styles = {
+    fontSize: "1.5em",
+    background: "rgba(192, 192, 192, 0.3)",
+    border: "none",
+    boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.2)",
+    borderRadius: "20px",
+    width: "100%",
+    margin: "20px"
+  };
+  return (
+    <button
+      style={styles}
+      className="legend-button"
+      onClick={handleLegendChange}
+    >
+      Show Legend
+    </button>
+  );
+};
 
 LegendButton.propTypes = {
   handleLegendChange: PropTypes.func.isRequired
-}
+};
 
 export default LegendButton;

911cf8dcb7b827028122f6b37f914da8fd9d1cc0
halfalpine <rocksandwoks@gmail.com>
2017-09-22 00:16:39 -0400

    Add styles to Output

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
 
-const Output = ({text}) =>
-  <div className="md-component">
+import { TextColumn, TextDisplay } from './Styles';
+
+const Output = ({ text }) =>
+  <TextColumn>
     <h2>This is the Output.</h2>
-    <div
-      className="textarea-output"
-      dangerouslySetInnerHTML={{__html: marked(text, {breaks: true})}}
+    <TextDisplay
+      dangerouslySetInnerHTML={{
+        __html: marked(text, { breaks: true })
+      }}
     />
-  </div>
+  </TextColumn>
 
 Output.propTypes = {
   text: PropTypes.string.isRequired

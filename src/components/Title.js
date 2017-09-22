bc8fb988ed779587cdfe50125e0beedfd1707d0d
halfalpine <rocksandwoks@gmail.com>
2017-09-21 23:33:53 -0400

    Add styles for Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  margin: 30px 0px;
+  font-size 1.2em;
+`;
+
+const Headline = styled.h1`
+  text-align: center;
+  margin-bottom: 0px;
+`;
+
+const Subhead = styled.p`
+  text-align: center;
+`;
 
 const Title = () => (
-  <div className="page-title">
-    <h1>Markdown Previewer</h1>
-    <p>A React project for <a href="https://www.freecodecamp.org/challenges/build-a-markdown-previewer">Free Code Camp</a></p>
-  </div>
+  <Container>
+    <Headline>Markdown Previewer</Headline>
+    <Subhead>A React project for <a href="https://www.freecodecamp.org/challenges/build-a-markdown-previewer">Free Code Camp</a></Subhead>
+  </Container>
 )
 
 export default Title;

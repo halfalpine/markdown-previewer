c5d333ada0df40eb473ac9753a9c5f3e691eba23
halfalpine <rocksandwoks@gmail.com>
2017-09-22 00:10:46 -0400

    Add styles for Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Input = ({text, handleInputChange}) =>
-  <div className="md-component">
+import { TextColumn, TextDisplay } from './Styles';
+
+const FormDisplay = TextDisplay.withComponent('form');
+
+const TextArea = styled.textarea.attrs({
+  type: 'text',
+  autoFocus: false,
+}) `
+  height: 100%;
+  width: 100%;
+  font: inherit;
+  font-family: monospace;
+  border: none;
+  outline: none;
+  resize: none;
+`;
+
+const Input = ({ text, handleInputChange }) =>
+  <TextColumn>
     <h2>This is the Input.</h2>
-    <form className="textarea-input">
-      <textarea
-        autoFocus={false}
-        type="text"
+    <FormDisplay>
+      <TextArea
         value={text}
         onChange={handleInputChange}
       >
-      </textarea>
-    </form>
-  </div>
+      </TextArea>
+    </FormDisplay>
+  </TextColumn>
 
 Input.propTypes = {
   text: PropTypes.string.isRequired,
---
+++ src/components/Styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const TextColumn = styled.div`
+    width: 50%;
+    padding: 10px;
+`;
+
+export const TextDisplay = styled.div`
+    height: 33vh;
+    background: white;
+    border: 1px solid black;
+    font-size: 18px;
+    padding: 0px;
+`;

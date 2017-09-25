45c13680b585ac61bc5f9a79c3d146fc0677d291
Dirk Peters <rocksandwoks@gmail.com>
2017-09-25 16:52:06 -0400

    Add placeholder text to form

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import { TextColumn, TextDisplay } from './Styles';
+import { TextColumn, TextDisplay } from "./Styles";
 
-const FormDisplay = TextDisplay.withComponent('form');
+const FormDisplay = TextDisplay.withComponent("form");
 
 const TextArea = styled.textarea.attrs({
-  type: 'text',
-  autoFocus: false,
-}) `
+  type: "text",
+  autoFocus: false
+})`
   height: 100%;
   width: 100%;
   font: inherit;
@@ -19,21 +19,22 @@ const TextArea = styled.textarea.attrs({
   resize: none;
 `;
 
-const Input = ({ text, handleInputChange }) =>
+const Input = ({ text, handleInputChange }) => (
   <TextColumn>
     <h2>This is the Input.</h2>
     <FormDisplay>
       <TextArea
+        placeholder="Text goes here."
         value={text}
         onChange={handleInputChange}
-      >
-      </TextArea>
+      />
     </FormDisplay>
   </TextColumn>
+);
 
 Input.propTypes = {
   text: PropTypes.string.isRequired,
   handleInputChange: PropTypes.func.isRequired
-}
+};
 
 export default Input;
--- src/reducers/text.js
+++ src/reducers/text.js
@@ -1,12 +1,12 @@
-const UPDATE_TEXT = 'UPDATE_TEXT';
+const UPDATE_TEXT = "UPDATE_TEXT";
 
-const text = (state = 'foo', action) => {
+const text = (state = "", action) => {
   switch (action.type) {
     case UPDATE_TEXT:
       return action.text;
     default:
       return state;
   }
-}
+};
 
 export default text;
